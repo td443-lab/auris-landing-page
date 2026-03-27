272cffd413c467b47435630c808424c7e2b83d85
kimdangportfolio <kimdang1710@gmail.com>
2026-03-27 05:40:49 -0400

    Connect Google Sheets waitlist

--- config.js
+++ config.js
@@ -1,3 +1,3 @@
 window.AERA_CONFIG = {
-  waitlistEndpoint: ""
+  waitlistEndpoint: "https://script.google.com/macros/s/AKfycbwugfRcUW3kc1Q1SkrHardnhpbYOoaYkEFPeHIjvy7hh8Ut5etRdx3ALeyqrEMZaogh6Q/exec"
 };
